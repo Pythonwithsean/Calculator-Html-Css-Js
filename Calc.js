70ff984cb25ec04f55d2c758df0ad9bf74fbcc4e
Lunafcs <Lunafcs@protonmail.com>
2023-09-08 13:51:19 +0100

    clear function

--- Calc.js
+++ Calc.js
@@ -4,5 +4,16 @@ const calcScreen = document.querySelector(".screen")
 for(let i = 0; i < button.length; i++){
    button[i].addEventListener("click",()=>{
       calcScreen.innerHTML += button[i].textContent;
+      if(button[i].textContent === "C"){
+         clear(calcScreen)
+      }
    })
+}
+
+const clear  = (calcElement) => {
+   calcElement.innerHTML = ""
+}
+
+const Add = (calcElement) => {
+   
 }
